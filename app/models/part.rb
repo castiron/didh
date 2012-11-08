3f6c227de76f54c5abef7222a4ef8c63a0d4e2b7
Zach Davis <zach@castironcoding.com>
2012-11-07 21:32:46 -0700

    Made parts views more bootstrappy

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,4 +1,4 @@
 class Part < ActiveRecord::Base
-  attr_accessible :label, sorting
+  attr_accessible :label, :sorting
   has_many :chapters
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121108025821) do
+ActiveRecord::Schema.define(:version => 20121108032409) do
 
   create_table "chapters", :force => true do |t|
     t.string   "label"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20121108025821) do
     t.string   "label"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "sorting"
   end
 
 end
