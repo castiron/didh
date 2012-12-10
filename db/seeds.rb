08df805ebdb3e5e481c01a788cf93c5a71f08fc1
Zach Davis <zach@castironcoding.com>
2012-12-10 09:01:03 -0800

    [FEATURE] Added seed data to app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+parts = Part.create([
+	{ label: 'Introduction', sorting: 0 },
+	{ label: 'Part I. Defining the Digital Humanities', sorting: 1 },
+	{ label: 'Part II. Theorizing the Digital Humanities', sorting: 2 },
+	{ label: 'Part III. Critiquing the Digital Humanities', sorting: 3 },
+	{ label: 'Part IV. Practicing the Digital Humanities', sorting: 4 },
+	{ label: 'Part V. Teaching the Digital Humanities', sorting: 5 },
+	{ label: 'Part VI. Envisioning the Future of the Digital Humanities', sorting: 6 }
+])
