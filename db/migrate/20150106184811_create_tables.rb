e3c8e60c3d0af429659400e50510412051d40586
Zach Davis <zach@castironcoding.com>
2015-01-06 10:55:26 -0800

    [CHORE] One migration to rule them all, one migration to bind them

---
+++ db/migrate/20150106184811_create_tables.rb
@@ -0,0 +1,130 @@
+class CreateTables < ActiveRecord::Migration
+  def up
+
+    create_table "admins", :force => true do |t|
+      t.string   "email",                  :default => "", :null => false
+      t.string   "encrypted_password",     :default => "", :null => false
+      t.string   "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.integer  "sign_in_count",          :default => 0
+      t.datetime "current_sign_in_at"
+      t.datetime "last_sign_in_at"
+      t.string   "current_sign_in_ip"
+      t.string   "last_sign_in_ip"
+      t.datetime "created_at",                             :null => false
+      t.datetime "updated_at",                             :null => false
+    end
+
+    add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
+    add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
+
+    create_table "annotations", :force => true do |t|
+      t.integer  "text_id"
+      t.integer  "sentence"
+      t.string   "ip"
+      t.integer  "type"
+      t.datetime "created_at", :null => false
+      t.datetime "updated_at", :null => false
+    end
+
+    add_index "annotations", ["text_id"], :name => "index_annotations_on_text_id"
+
+    create_table "authors", :force => true do |t|
+      t.string "name"
+      t.string "institution"
+    end
+
+    create_table "authors_texts", :id => false, :force => true do |t|
+      t.integer "text_id",   :null => false
+      t.integer "author_id", :null => false
+    end
+
+    create_table "chapters", :force => true do |t|
+      t.string   "label"
+      t.datetime "created_at", :null => false
+      t.datetime "updated_at", :null => false
+      t.integer  "part_id"
+    end
+
+    create_table "comments", :force => true do |t|
+      t.integer  "user_id"
+      t.integer  "parent_id"
+      t.integer  "text_id"
+      t.text     "body"
+      t.datetime "created_at",        :null => false
+      t.datetime "updated_at",        :null => false
+      t.string   "author_name"
+      t.string   "author_email"
+      t.integer  "sentence_checksum"
+    end
+
+    create_table "editions", :force => true do |t|
+      t.string   "label"
+      t.datetime "created_at", :null => false
+      t.datetime "updated_at", :null => false
+      t.integer  "sorting"
+    end
+
+    create_table "keywords", :force => true do |t|
+      t.string   "word"
+      t.datetime "created_at", :null => false
+      t.datetime "updated_at", :null => false
+      t.integer  "sentence"
+      t.integer  "text_id"
+    end
+
+    create_table "parts", :force => true do |t|
+      t.string   "label"
+      t.datetime "created_at", :null => false
+      t.datetime "updated_at", :null => false
+      t.integer  "sorting"
+    end
+
+    create_table "sentences", :force => true do |t|
+      t.text     "body"
+      t.integer  "text_id"
+      t.datetime "created_at", :null => false
+      t.datetime "updated_at", :null => false
+      t.integer  "checksum"
+    end
+
+    create_table "texts", :force => true do |t|
+      t.text     "body"
+      t.datetime "created_at",   :null => false
+      t.datetime "updated_at",   :null => false
+      t.string   "title"
+      t.text     "abstract"
+      t.text     "notes"
+      t.text     "bibliography"
+      t.string   "source_file"
+      t.integer  "part_id"
+      t.integer  "edition_id"
+      t.integer  "sorting"
+      t.boolean  "is_static"
+    end
+
+    create_table "users", :force => true do |t|
+      t.string   "email",                  :default => "", :null => false
+      t.string   "encrypted_password",     :default => "", :null => false
+      t.string   "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.integer  "sign_in_count",          :default => 0
+      t.datetime "current_sign_in_at"
+      t.datetime "last_sign_in_at"
+      t.string   "current_sign_in_ip"
+      t.string   "last_sign_in_ip"
+      t.datetime "created_at",                             :null => false
+      t.datetime "updated_at",                             :null => false
+      t.string   "name"
+      t.string   "alias"
+      t.boolean  "admin"
+    end
+
+    add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+    add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  end
+
+end
