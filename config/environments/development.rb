e262f3b430365fcaeab4ecd48afa063f8f403bc3
Zach Davis <zach@castironcoding.com>
2015-04-07 16:33:53 -0700

    [CHORE] Adds log level to config files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,8 @@ Didh::Application.configure do
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
+  config.log_level = :debug
+
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,6 +20,8 @@ Didh::Application.configure do
   # Raise exceptions instead of rendering exception templates
   config.action_dispatch.show_exceptions = false
 
+  config.log_level = :debug
+
   # Disable request forgery protection in test environment
   config.action_controller.allow_forgery_protection    = false
 
