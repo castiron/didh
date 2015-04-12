b3d27f7989182a4acf4b855ceaff5c1e638de22e
Zach Davis <zach@castironcoding.com>
2015-04-12 11:51:08 -0700

    [CHORE] Finalizes production deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,12 @@
 set :branch, "master"
-role :app, %w{dhdebates-webserver.ciclabs.com}
-role :web, %w{dhdebates-webserver.ciclabs.com}
-role :db,  %w{dhdebates-webserver.ciclabs.com}
+set :server_name, "dhdebates-webserver.ciclabs.com"
 
+set :stage, :production
+set :rails_env, :production
+
+role :app, %w{dhdebates@dhdebates-webserver.ciclabs.com}
+role :web, %w{dhdebates@dhdebates-webserver.ciclabs.com}
+role :db,  %w{dhdebates@dhdebates-webserver.ciclabs.com}
+
+set :deploy_to, "~/#{fetch(:application)}"
 
