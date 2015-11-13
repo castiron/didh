50bdef419dbca46591ed215159dbcfa5bd65717f
Lucas Thurston <lucas@castironcoding.com>
2015-11-13 12:59:12 -0800

    [CHORE] Setup cap for rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,11 @@ set :deploy_to, '/home/dhdebates/dhdebates'
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :linked_files, ["config/secrets.yml"]
 set :keep_releases, 5
+set :rbenv_type, :user
+set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
 
 namespace :deploy do
 
