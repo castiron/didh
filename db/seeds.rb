4727b6d694ffe19031587374a172fd5f0f561552
Lucas Thurston <lucas@castironcoding.com>
2016-04-30 15:22:43 -0700

    [C] Make db:seed able to be run again

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,21 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-raise "You probably don't really want to seed again; you already have editions in the db!" if Edition.count > 0
+e1 = Edition.where(label: '2012 Print Edition', sorting: 100).first_or_create
+e2 = Edition.where(label: '2016 Print Edition', sorting: 99).first_or_create
 
-Edition.create([
-  { label: '2012 Print Edition'},
-  { label: '2016 Print Edition'}
-])
+Part.where(label: 'Introduction', sorting: 0, edition: e1).first_or_create
+Part.where(label: 'Part I. Defining the Digital Humanities', sorting: 1, edition: e1).first_or_create
+Part.where(label: 'Part II. Theorizing the Digital Humanities', sorting: 2, edition: e1).first_or_create
+Part.where(label: 'Part III. Critiquing the Digital Humanities', sorting: 3, edition: e1).first_or_create
+Part.where(label: 'Part IV. Practicing the Digital Humanities', sorting: 4, edition: e1).first_or_create
+Part.where(label: 'Part V. Teaching the Digital Humanities', sorting: 5, edition: e1).first_or_create
+Part.where(label: 'Part VI. Envisioning the Future of the Digital Humanities', sorting: 6, edition: e1).first_or_create
 
-# 1st edition parts
-Part.create([
-	{ label: 'Introduction', sorting: 0, edition_id: 1 },
-	{ label: 'Part I. Defining the Digital Humanities', sorting: 1, edition_id: 1 },
-	{ label: 'Part II. Theorizing the Digital Humanities', sorting: 2, edition_id: 1 },
-	{ label: 'Part III. Critiquing the Digital Humanities', sorting: 3, edition_id: 1 },
-	{ label: 'Part IV. Practicing the Digital Humanities', sorting: 4, edition_id: 1 },
-	{ label: 'Part V. Teaching the Digital Humanities', sorting: 5, edition_id: 1 },
-	{ label: 'Part VI. Envisioning the Future of the Digital Humanities', sorting: 6, edition_id: 1 }
-])
-
-# 2nd edition parts
-Part.create([
-	{ label: 'Introduction', sorting: 0, edition_id: 2 },
-	{ label: 'Histories and Futures of the Digital Humanities', sorting: 1, edition_id: 2 },
-	{ label: 'Digital Humanities and Its Methods', sorting: 2, edition_id: 2 },
-	{ label: 'Digital Humanities and Its Practices', sorting: 3, edition_id: 2 },
-	{ label: 'Digital Humanities and the Disciplines', sorting: 4, edition_id: 2 },
-	{ label: 'Digital Humanities and Its Critics', sorting: 5, edition_id: 2 },
-	{ label: 'Forum: Text Analysis at Scale', sorting: 6, edition_id: 2 }
-])
+Part.where(label: 'Introduction', sorting: 0, edition: e2).first_or_create
+Part.where(label: 'Histories and Futures of the Digital Humanities', sorting: 1, edition: e2).first_or_create
+Part.where(label: 'Digital Humanities and Its Methods', sorting: 2, edition: e2).first_or_create
+Part.where(label: 'Digital Humanities and Its Practices', sorting: 3, edition: e2).first_or_create
+Part.where(label: 'Digital Humanities and the Disciplines', sorting: 4, edition: e2).first_or_create
+Part.where(label: 'Digital Humanities and Its Critics', sorting: 5, edition: e2).first_or_create
+Part.where(label: 'Forum: Text Analysis at Scale', sorting: 6, edition: e2).first_or_create
